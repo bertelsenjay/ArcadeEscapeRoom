518b1377e15250f799b0a5408538df2dc3af72ea
Cooper Carey <65479428+CooperCareyGameDev@users.noreply.github.com>
2023-11-15 10:26:54 -0800

    Started Virtual Joystick

---
+++ ArcadeEscape/Assets/Scripts/JoystickControl.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickControl : MonoBehaviour
+{
+    public Transform topOfJoystick;
+
+    [SerializeField] private float forwardBackwardTilt;
+    [SerializeField] private float sideToSideTilt;
+
+    void Update()
+    {
+        
+    }
+}
