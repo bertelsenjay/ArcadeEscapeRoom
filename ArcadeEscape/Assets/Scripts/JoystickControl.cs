bdf3a5cf016560547533ee03473b858ca54e37a4
Cooper Carey <65479428+CooperCareyGameDev@users.noreply.github.com>
2023-11-16 08:24:58 -0800

    Update JoystickControl.cs

--- ArcadeEscape/Assets/Scripts/JoystickControl.cs
+++ ArcadeEscape/Assets/Scripts/JoystickControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class JoystickControl : MonoBehaviour
 {
@@ -11,6 +12,11 @@ public class JoystickControl : MonoBehaviour
 
     void Update()
     {
-        
+        forwardBackwardTilt = topOfJoystick.rotation.eulerAngles.x; 
+        if (forwardBackwardTilt < 355 && forwardBackwardTilt > 290)
+        {
+            forwardBackwardTilt = Math.Abs(forwardBackwardTilt - 360);
+        }
+
     }
 }
