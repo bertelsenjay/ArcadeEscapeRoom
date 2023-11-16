e44b6288ca90bb3358e8f5dc891dde441883c15b
Cooper Carey <65479428+CooperCareyGameDev@users.noreply.github.com>
2023-11-16 10:23:45 -0800

    shooting targets

---
+++ ArcadeEscape/Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Target"))
+        {
+            Destroy(collision.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
