d298b80095c52dabda3f8f8cbd35d8d6c6994d3a
Cooper Carey <65479428+CooperCareyGameDev@users.noreply.github.com>
2023-11-21 09:54:28 -0800

    Whack a mole functionality

--- ArcadeEscape/Assets/Scripts/GameController.cs
+++ ArcadeEscape/Assets/Scripts/GameController.cs
@@ -6,12 +6,16 @@ using TMPro;
 public class GameController : MonoBehaviour
 {
     public TextMeshProUGUI timerText;
-    public float gameTimer = 30f; 
+    public float gameTimer = 30f;
 
+    public GameObject moleContainer;
+    private Mole[] moles;
+    public float showMoleTimer = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        moles = moleContainer.GetComponentsInChildren<Mole>();
+        Debug.Log(moles.Length);
     }
 
     // Update is called once per frame
@@ -21,6 +25,13 @@ public class GameController : MonoBehaviour
         if (gameTimer > 0f)
         {
             timerText.text = "Time: " + Mathf.Floor(gameTimer);
+            showMoleTimer -= Time.deltaTime;
+            if (showMoleTimer < 0f)
+            {
+                moles[Random.Range(0, moles.Length)].ShowMole();
+
+                showMoleTimer = 1.5f;
+            }
         }
         else
         {
