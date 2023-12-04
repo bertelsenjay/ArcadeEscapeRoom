aecb4c172ef4e6b956db719fbc38ea3cb366cb89
Cooper Carey <65479428+CooperCareyGameDev@users.noreply.github.com>
2023-12-04 08:36:11 -0800

    Update PauseMenu.cs

--- ArcadeEscape/Assets/Scripts/PauseMenu.cs
+++ ArcadeEscape/Assets/Scripts/PauseMenu.cs
@@ -11,10 +11,12 @@ public class PauseMenu : MonoBehaviour
      
     public bool isCanvasActive = true;
     LocomotionSystem locomotionSystem; 
+    TeleportationProvider teleportationProvider;
     // Start is called before the first frame update
     void Start()
     {
-        locomotionSystem = FindObjectOfType<LocomotionSystem>()
+        locomotionSystem = FindObjectOfType<LocomotionSystem>();
+        teleportationProvider = FindObjectOfType<TeleportationProvider>();
         DisplayUI();
     }
 
@@ -25,14 +27,16 @@ public class PauseMenu : MonoBehaviour
         if (isCanvasActive)
         {
             canvas.SetActive(false);
-            
+            //locomotionSystem.enabled = true;
+            teleportationProvider.enabled = true;
             isCanvasActive = false;
             Time.timeScale = 1;
         }
         else if (!isCanvasActive)
         {
             canvas.SetActive(true);
-            
+            //locomotionSystem.enabled = false; 
+            teleportationProvider.enabled = false;
             isCanvasActive = true;
             Time.timeScale = 0; 
         }
@@ -47,5 +51,13 @@ public class PauseMenu : MonoBehaviour
     public void QuitGame()
     {
         SceneManager.LoadScene("MainMenu");
-    } 
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            DisplayUI();
+        }
+    }
 }
